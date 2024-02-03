cdc9c7ef8ed35bed6b26da084e0ba6657c0a5b90
HojinLee <hojin5633@gmail.com>
2024-02-03 10:37:21 +0900

    Chapter4 Fiber 구조 고민중

--- src/index.jsx
+++ src/index.jsx
@@ -46,10 +46,84 @@ function render(element, container) {
 
   Object.entries(props).forEach(([key, value]) => (dom[key] = value));
 
-  children.forEach((child) => render(child, dom));
-
   container.appendChild(dom);
+
+  return { dom };
 }
+/**
+ * 1. requestIdle을 쓰긴하지만, 브라우저가 계속 쉬지 않고 일을 한다면 계속 블럭되면 안된다.
+ * 2. 최소 기다리는 시간(데드라인)이 있어야한다.
+ * 3. requestIdleCallback -> task queue, micro task
+ * 4. deadline -> 시간
+ */
+
+// 부모가 무조건 먼저 렌더링 되어야한다
+// DFS
+let nextUnitOfWork: fiver = null;
+// { dom, children }
+
+// 1 순위. child[0]
+// 2 순위. 내 sibling
+// 3 순위. parent Sibling
+// return {
+//   type: "TEXT_ELEMENT",
+//   props: {
+//     nodeValue: text,
+//     children: [],
+//   },
+//   dom : Element
+//   parentFiber: Fiber (parentDom + parentSibling을 얻기 위해서)
+//   childINdex
+// };
+// 1. performUnitOfWork 실행.
+// 2. render 1개만
+// 3. nextUnitOfWork를 생성해야한다.
+function performUnitOfWork(nextUnitOfWork) {
+  const dom = render(nextUnitOfWork); // parentdom.appendchild(dom)
+  nextUnitOfWork.dom = dom;
+
+  // A -> B(C) -> E(C) => F(K)
+  //                   => K(G)
+  //           -> G(C)
+  //   -> C(D)
+  //   -> D()
+
+  // A -> B( ,0) -> E(C,0) => F(G,0)
+  //                       => K(G,1)
+  //             -> G(C,1)
+  //   -> C( ,1)
+  //   -> D( ,2)
+
+  // 1) child[0]
+  let nextFiber = null;
+
+  if (children.length > 1) {
+    nextFiber = createFiber(element.props.children[0]);
+  } else if (children[index + 1]) {
+    // 2) next sibling
+    nextFiber = createFiber(nextUnitOfWork.chidren[index + 1]);
+  } else if (nextUnitOfWork.parentFiber.nextSibling) {
+    // 3) parent sibling
+    nextFiber = createFiber(nextUnitOfWork.parentFiber.nextSibling);
+  }
+
+  nextFiber && (nextFiber.parent = nextUnitOfWork);
+
+  return nextFiber;
+}
+// TODO : 01/31 , createFiber, nextSibling 할당하기
+
+function workLoop(deadline) {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
 
 const Didact = {
   createElement,
